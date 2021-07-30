e9634795dc2cdbe6b1a8e8714ffa2ac97b5fbbf8
SakuyaQwQ <1092705065@qq.com>
2021-07-30 14:23:20 +0800

    a

--- demo/src/main/java/com/example/demo/Controller/LoginController.java
+++ demo/src/main/java/com/example/demo/Controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.example.demo.Controller;
+package com.example.demo.controller;
 
 import com.example.demo.dao.UserDao;
 import com.example.demo.entity.User;
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.regex.*;
 
 @RestController
 public class LoginController {
@@ -17,8 +18,8 @@ public class LoginController {
     @PostMapping("/login")
     public String login(@RequestBody User user) {
         System.out.println(user);
-        if (userDao.getUser(user.getAccount()) != null
-                && userDao.getUser(user.getAccount()).getPassword().equals(user.getPassword())) {
+        User userInDB = userDao.getUser(user.getAccount());
+        if (userInDB != null && userInDB.getPassword().equals(user.getPassword())) {
             return "ok";
         } else {
             return "error";
@@ -27,9 +28,17 @@ public class LoginController {
 
     @PostMapping("/register")
     public String register(@RequestBody User user) {
-        try{
-            userDao.addUser(user);            
-        }catch(Exception e){
+        String validAccount = "^[0-9a-zA-Z]{6,12}";
+        String validPassword = "^(([~!@#$%^&*()_+|<>,.?/:;'\\[\\]{}\"])|([a-zA-Z0-9])){6,12}";
+        if (!Pattern.matches(validAccount,user.getAccount())) {
+            return "error_account";
+        }
+        if (!Pattern.matches(validPassword,user.getPassword())) {
+            return "error_password";
+        }
+        try {
+            userDao.addUser(user);
+        } catch (Exception e) {
             return "error";
         }
         return "ok";
